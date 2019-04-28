18810b326140c155069643a0b2dde5ddf168e101
iftekharbhuiyan <ib.nadim@gmail.com>
2019-04-27 20:42:09 -0400

    Default functions file of a Theme.

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * set custom footer message
+ * using "admin_footer_text" hook
+ * WP Codex: https://developer.wordpress.org/reference/hooks/admin_footer_text/
+ */
+add_filter('admin_footer_text','custom_admin_footer_text');
+function custom_admin_footer_text() {
+	echo '<p>This is a custom text.</p>';
+}
+?>

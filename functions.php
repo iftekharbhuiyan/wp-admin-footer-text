79bbe7062606897b0b4003bd3a3e05aafeef7637
Iftekhar Bhuiyan <ibhuiyan@mail.com>
2024-04-26 12:58:29 -0400

    formatting update

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@
  * using "admin_footer_text" hook
  * WP Codex: https://developer.wordpress.org/reference/hooks/admin_footer_text/
  */
-add_filter('admin_footer_text','custom_admin_footer_text');
 function custom_admin_footer_text() {
-	echo '<p>This is a custom text.</p>';
+    echo '<p>This is a custom text.</p>';
 }
+add_filter('admin_footer_text','custom_admin_footer_text');
 ?>
